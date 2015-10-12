9030dbe607a0bb7d4a71cef0ab180531e110aba3
lixx3899 <lixx3899@umn.edu>
2015-10-12 16:20:04 -0500

    add project iter 1 files and matchToken.cpp

---
+++ ProjectFiles/Iter_1_Files/src/matchToken.cpp
@@ -0,0 +1,33 @@
+int matchTokenType (const char * input_str, tokenType type) {
+    // given input string and type, return the length of the match
+    int match_length = 0
+    switch (type) {
+        case intKwd: // something
+            break;
+        case floatKwd: 
+    }
+    
+    // if doesn't match, return 0
+    return match_length;
+}
+
+class Token * matchToken(const char * input_str) {
+    // for all types, find the matched one that gives the maximum length
+    
+    class Token *token_matched = new class Token; 
+    int max_length = -1;
+    tokenType type_matched = last;
+    
+    for (int index = 0; index != last ; index++)
+        tokenType type = static_cast<tokenType>(index);
+        int length = matchTokenType(input_str, type);
+        if (length > max_length) {
+            max_length = length;
+            type_matched = type;
+        } else if (length == max_length) {
+            cout << "conflict happens between " << type << "and "  << type_matched << endl;
+            return NULL;
+        }
+    }
+    return matched;
+}
---
+++ ProjectFiles/Iter_1_Files/src/scanner.h
@@ -0,0 +1,50 @@
+/* A small meaningless comment */
+#ifndef SCANNER_H
+#define SCANNER_H
+
+#include <regex.h>
+#include <string>
+
+class Token ;
+
+/* This enumerated type is used to keep track of what kind of
+   construct was matched. 
+ */
+
+enum tokenEnumType { 
+
+	intKwd, floatKwd,  stringKwd, matrixKwd,
+	letKwd, inKwd, endKwd, ifKwd, thenKwd, elseKwd,
+	repeatKwd, printKwd, toKwd,
+
+	// Constants
+	intConst, floatConst, stringConst, 
+
+	// Names
+	variableName ,
+
+	// Punctuation
+	leftParen, rightParen, 
+	leftCurly, rightCurly, 
+	leftSquare, rightSquare,
+
+	semiColon, colon,
+
+	//Operators
+	assign, 
+	plusSign, star, dash, forwardSlash,
+	lessThan, lessThanEqual, greaterThan, greaterThanEqual,
+	equalsEquals, notEquals,
+	andOp, orOp, notOp,
+
+	// Special terminal types
+	endOfFile ,
+	lexicalError
+} ;
+typedef enum tokenEnumType tokenType ;
+
+// Below you need to write your class definitions for Token and Scanner.
+
+
+
+#endif /* SCANNER_H */
