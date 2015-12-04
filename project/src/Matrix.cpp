d5872eede06bb24922717be6193860e9b6b518d1
Jingxiang Li <lixx3899@umn.edu>
2015-12-04 17:53:26 -0600

    completes the first version of Matrix, need test

---
+++ project/src/Matrix.cpp
@@ -0,0 +1,65 @@
+#include "./Matrix.h"
+#include <fstream>
+#include <iostream>
+
+matrix::matrix(int row, int col) : rows(row), cols(col) {
+    data = new float *[rows];
+    for (int i = 0; i != rows; i++) {
+        data[i] = new float[cols];
+    }
+}
+
+matrix::matrix(const matrix &m) : matrix(m.rows, m.cols) {
+    for (int i = 0; i != rows; i++)
+        for (int j = 0; j != cols; j++) {
+            data[i][j] = m[i][j];
+        }
+}
+
+int matrix::numRows() const { return rows; }
+
+int matrix::numCols() const { return cols; }
+
+float *matrix::operator[](int row) { return data[row]; }
+
+const float *matrix::operator[](int row) const { return data[row]; }
+
+std::ostream &operator<<(std::ostream &os, const matrix &m) {
+    for (int i = 0; i != m.numRows(); i++) {
+        for (int j = 0; j != m.numCols(); j++) {
+            os << m[i][j] << " ";
+        }
+        os << std::endl;
+    }
+    return os;
+}
+
+matrix operator*(const matrix &left, const matrix &right) {
+    matrix m(left.numRows(), right.numCols());
+    for (int i = 0; i != m.numRows(); i++) {
+        for (int j = 0; j != m.numCols(); j++) {
+            for (int k = 0; k != left.numCols(); k++) {
+                m[i][j] += left[i][k] * right[k][j];
+            }
+        }
+    }
+    return m;
+}
+
+matrix matrix::matrixRead(std::string filename) {
+    std::ifstream is(filename);
+
+    // read nrows and ncols
+    int nrows, ncols;
+    is >> nrows >> ncols;
+
+    // initiate matrix
+    matrix m(nrows, ncols);
+
+    // read all values
+    for (int i = 0; i != nrows; i++)
+        for (int j = 0; j != ncols; j++) {
+            is >> m[i][j];
+        }
+    return m;
+}
--- project/src/Matrix.h
+++ project/src/Matrix.h
@@ -7,28 +7,34 @@
 
 class matrix {
 public:
-    matrix(int i, int j) ;
-    matrix (const matrix& m) ;
+    matrix(int row, int col);
+    matrix(const matrix &m);
 
-    int numRows ( ) ;
-    int numCols ( ) ;
+    int numRows() const;
+    int numCols() const;
 
-    float *access(const int i, const int j) const ;
-    friend std::ostream& operator<<(std::ostream &os, matrix &m) ;
+    float *operator[](int row);
 
-    static matrix matrixRead ( std::string filename ) ;
+    const float *operator[](int row) const;
+
+    friend std::ostream &operator<<(std::ostream &os, const matrix &m);
+
+    friend matrix operator*(const matrix &left, const matrix &right);
+
+    static matrix matrixRead(std::string filename);
 
 private:
-    matrix() { }
-    int rows ;
-    int cols ;
+    // we don't implement matrix() {} ??
+    matrix() {}
+    int rows;
+    int cols;
 
     /* Your implementation of "data" may vary.  There are ways in
        which data can be an array of arrays and thus simplify the
        access method, at the cost of complicating the process of
        allocating space for data.  The choice is entirely up to
        you. */
-    float *data ;
-} ;
+    float **data;
+};
 
-#endif // MATRIX_H
+#endif  // MATRIX_H
