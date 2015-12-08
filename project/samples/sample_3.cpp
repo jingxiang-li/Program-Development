ca5565231045da62b42b5a51e55fe3c1758093a6
Jingxiang Li <lixx3899@umn.edu>
2015-12-07 19:08:46 -0600

    add translated cpp files

---
+++ project/samples/sample_3.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+#include "Matrix.h"
+
+int main () {
+int w;
+w = 4;
+int h;
+h = 5;
+int d;
+d = 6;
+int volume;
+volume = ({
+int area;
+area = w * h;
+area * d;
+});
+std::cout << volume;
+std::cout << "\n";
+}
