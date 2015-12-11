ff8201772a5526b3d9e4906b11970077072e0cad
lixx3899 <lixx3899@umn.edu>
2015-12-11 09:50:20 -0600

    completes clean button

--- project/src/mainwindow.cpp
+++ project/src/mainwindow.cpp
@@ -40,3 +40,26 @@ void MainWindow::on_makeButton_clicked()
     cMake->kill();
     delete cMake;
 }
+
+void MainWindow::on_cleanButton_clicked()
+{
+    QProcess *cMake = new QProcess(this);
+    QString file = QDir::currentPath() + "/../src/";
+    QStringList args;
+
+    args.append("-C");
+    args.append(file);
+    args.append("clean");
+
+    cMake->setProgram("//usr//bin//make");
+    cMake->setArguments(args);
+    cMake->setProcessChannelMode(QProcess::MergedChannels); //directs both std::out and std::error output to the same channel for readAll()
+    cMake->start();
+    cMake->waitForFinished();
+
+    QString output(cMake->readAll());
+    this->ui->makeOutputDisplay->setPlainText(output);
+
+    cMake->kill();
+    delete cMake;
+}
--- project/src/mainwindow.h
+++ project/src/mainwindow.h
@@ -23,6 +23,8 @@ public:
 private slots:
     void on_makeButton_clicked();
 
+    void on_cleanButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
